7039a065233f206fdebdab019c2838b535dd3d71
sanghyeop <xoxoxmin1268@naver.com>
2025-06-18 23:42:11 +0900

    feat: 좌석 관리 서비스 초기 구현 및 좌석 등록 API 생성

---
+++ src/main/kotlin/com/ticketing/seat_service/SeatServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.ticketing.seat_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeatServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeatServiceApplication>(*args)
+}
